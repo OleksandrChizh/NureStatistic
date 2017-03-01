2baf6f12ad4ef0e902f8d29a4c75566a428639e7
SanyaChizh <sasha.chizh96@gmail.com>
2017-03-01 14:54:23 +0200

    Created n-layer architecture

---
+++ NureStatistic/src/NureStatistic.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("NureStatistic.Core")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02cf19e4-10f8-4ae8-b3c0-f5f6438f5fa9")]
